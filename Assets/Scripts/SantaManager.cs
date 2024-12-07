babcd20a5b10c55d2366c6363d23e8f7ed4e5b0f
Marin <96381767+luximo55@users.noreply.github.com>
2024-12-07 20:27:36 +0100

    Santa background animation

--- Assets/Scripts/SantaManager.cs
+++ Assets/Scripts/SantaManager.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class SantaManager : MonoBehaviour
 {
     [SerializeField] ActionManager actionManager;
+    [SerializeField] GameObject santa;
 
+    private Transform transf;
+    private GameObject activeSanta;
     private float intervalTime;
 
+    private void Awake()
+    {
+        transf = GetComponent<Transform>();
+    }
     private void Start()
     {
         StartInterval();
     }
 
+    private void Update()
+    {
+        if(activeSanta != null && activeSanta.GetComponent<Transform>().localPosition.x == 17)
+        {
+            Destroy(activeSanta);
+        }
+    }
+
     private void StartInterval()
     {
-        intervalTime = Random.Range(30, 100);
+        intervalTime = 5;//Random.Range(30, 100);
         Debug.Log("Santa will intialize in " + intervalTime);
-        Invoke("IntializeSanta", intervalTime);
+        Invoke("InitializeSanta", intervalTime);
     }
 
     private void InitializeSanta()
     {
         Debug.Log("Santa is initialized");
+        activeSanta = Instantiate(santa, transf);
     }
-
 }
