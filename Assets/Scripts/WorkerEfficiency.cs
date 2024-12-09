868771d9bb4429a32849f02003844934209112a0
luximo55 <luximo3@gmail.com>
2024-12-09 15:36:21 +0100

    Added scoring system

--- Assets/Scripts/WorkerEfficiency.cs
+++ Assets/Scripts/WorkerEfficiency.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Presets;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WorkerEfficiency : MonoBehaviour
 {
     [SerializeField] private RectTransform progress;
+    [SerializeField] private Text presentsText;
     public float Efficiency = 100;
+    public float presents = 0;
+    public int score = 0;
 
     private void Update()
     {
@@ -20,5 +25,9 @@ public class WorkerEfficiency : MonoBehaviour
         }
         progress.localScale = new Vector3(Efficiency/100, progress.localScale.y, progress.localScale.z);
         Efficiency -= 1.5f * Time.deltaTime;
+
+        presents += (5 * (Efficiency/100)) * Time.deltaTime;
+        score = (int)presents;
+        presentsText.text = score.ToString();
     }
 }
